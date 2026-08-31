b543ea78c42e9b6ea2f5475a0dffa1c43d1bdb11
ariana <arianakhodakaramzadeh99@gmail.com>
2026-08-31 20:57:28 +0330

    a

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import { MdDirectionsBike } from "react-icons/md";
 const Header = () => {
   return (
     <div>
+    <h1>da</h1>
       <div className='bg-green-950  w-[430px] h-[28px] md:w-[1400px] h-[50px] text-white'>This is a sample UI platform designed exclusively for testing and demo.</div>
     </div>
   )
